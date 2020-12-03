6227f7b568e254d314926855380794ce08bc06d6
Roman Drahan <roman@dragan.com.ua>
2020-12-03 12:31:24 +0200

    Fixes for Android & bump up to 73.0.1

--- src/angular/index.d.ts
+++ src/angular/index.d.ts
@@ -1,4 +1,9 @@
+import * as i0 from "@angular/core";
 export declare class YoutubePlayerDirective {
+    static ɵfac: i0.ɵɵFactoryDef<YoutubePlayerDirective, never>;
+    static ɵdir: i0.ɵɵDirectiveDefWithMeta<YoutubePlayerDirective, "YoutubePlayer", never, {}, {}, never>;
 }
 export declare class YoutubePlayerModule {
+    static ɵmod: i0.ɵɵNgModuleDefWithMeta<YoutubePlayerModule, [typeof YoutubePlayerDirective], never, [typeof YoutubePlayerDirective]>;
+    static ɵinj: i0.ɵɵInjectorDef<YoutubePlayerModule>;
 }
